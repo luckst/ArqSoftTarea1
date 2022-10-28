074c4a80c8515bdc0e6b83ab42664756bdfe34de
Lucas Arenas <lucario_9@hotmail.com>
2022-10-27 20:58:52 -0500

    Creacion capas

---
+++ ArqSoftware.CRUD/ArqSoftware.CRUD.Data/CrudContext.cs
@@ -0,0 +1,40 @@
+﻿using ArqSoftware.CRUD.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace ArqSoftware.CRUD.Data
+{
+    public class CrudContext: DbContext
+    {
+        public CrudContext()
+        {
+        }
+
+        public CrudContext(DbContextOptions<CrudContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                var env = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+
+                var config = new ConfigurationBuilder()
+                    .SetBasePath(Directory.GetCurrentDirectory())
+                    .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                    .AddJsonFile($"appsettings.{env}.json", optional: true)
+                    .Build();
+
+                optionsBuilder.UseSqlServer(config.GetConnectionString("DefaultConnection"));
+            }
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Client>().ToTable("Clients");
+        }
+    }
+}
---
+++ ArqSoftware.CRUD/ArqSoftware.CRUD.Models/Client.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ArqSoftware.CRUD.Models
+{
+    public class Client
+    {
+        [Key]
+        public int Id { get; set; }
+        public string DocumentNumber { get; set; }
+        public string LastName { get; set; }
+        public string FirstMidName { get; set; }
+        public string Email { get; set; }
+        public DateTime CreationDate { get; set; }
+    }
+}
